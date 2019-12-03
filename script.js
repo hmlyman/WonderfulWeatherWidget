1e033eaa7824de88e2dfb0def1040c3f6f5135bb
hmlyman <56166746+hmlyman@users.noreply.github.com>
2019-12-03 09:36:55 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+var weatherData;
+var request = new XMLHttpRequest();
+var date = new Date();
+
+function loadData() {
+    
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/daily/?q=Salt+Lake+City,us&units=imperial&cnt=5&appid=846d3b48355b6e95813bed8fb29f0fb3', true);
+    request.onload = loadComplete;
+    request.send();
+}
+
+loadData();
+
+function loadComplete(evt) {
+    weatherData = JSON.parse(evt.currentTarget.responseText);
+    var forecastedDaysArray = weatherData.list;
+    for(index in forecastedDaysArray){
+        var individualDayForecast = forecastedDaysArray[index];
+        individualDayForecast.date = new Date(individualDayForecast.dt*1000);
+        delete individualDayForecast.dt;
+        console.log("individualDayForecast.date: "+individualDayForecast.date);
+        console.log(individualDayForecast);
+    }
+    console.log(weatherData);
+
+    var iconCode = weatherData.list[0].weather[0].icon;
+    var iconPath = "http://openweathermap.org/img/w/"+iconCode+".png";
+    let link= document.getElementById("place");
+    link.href= "https://www.visitsaltlake.com/";
+    document.getElementById("place").innerHTML = weatherData.city.name;
+    document.getElementById("day").innerHTML = (date.getMonth()+1) + "/" + date.getDate();
+    document.getElementById("currentTemp").innerHTML = weatherData.list[0].temp.day+"&deg;";
+    document.getElementById("conditions").src = iconPath;
+    document.getElementById("conditionsDesc").innerHTML = weatherData.list[0].weather[0].description;
+    
+    var iconCode = weatherData.list[1].weather[0].icon;
+    var iconPath = "http://openweathermap.org/img/w/"+iconCode+".png";
+    document.getElementById('day2').innerHTML = (date.getMonth()+1) + "/" + date.getDate();
+    document.getElementById('conditions2').src = iconPath;
+    document.getElementById('conditionsDesc2').innerHTML = weatherData.list[1].weather[0].description;
+    document.getElementById("minTemp2").innerHTML = weatherData.list[1].temp.max+"&deg;";
+    document.getElementById("maxTemp2").innerHTML = weatherData.list[1].temp.min+"&deg;";
+
+    var iconCode = weatherData.list[2].weather[0].icon;
+    var iconPath = "http://openweathermap.org/img/w/"+iconCode+".png";
+    document.getElementById('day3').innerHTML = (date.getMonth()+1) + "/" +date.getDate();
+    document.getElementById('conditions3').src = iconPath;
+    document.getElementById('conditionsDesc3').innerHTML = weatherData.list[2].weather[0].description;
+    document.getElementById("minTemp3").innerHTML = weatherData.list[2].temp.max+"&deg;";
+    document.getElementById("maxTemp3").innerHTML = weatherData.list[2].temp.min+"&deg;";
+
+    var iconCode = weatherData.list[3].weather[0].icon;
+    var iconPath = "http://openweathermap.org/img/w/"+iconCode+".png";
+    document.getElementById('day4').innerHTML = (date.getMonth()+1) + "/" +date.getDate();
+    document.getElementById('conditions4').src = iconPath;
+    document.getElementById('conditionsDesc4').innerHTML = weatherData.list[3].weather[0].description;
+    document.getElementById("minTemp4").innerHTML = weatherData.list[3].temp.max+"&deg;";
+    document.getElementById("maxTemp4").innerHTML = weatherData.list[3].temp.min+"&deg;";
+
+    var iconCode = weatherData.list[4].weather[0].icon;
+    var iconPath = "http://openweathermap.org/img/w/"+iconCode+".png";
+    document.getElementById('day5').innerHTML = (date.getMonth()+1) + "/" +date.getDate();
+    document.getElementById('conditions5').src = iconPath;
+    document.getElementById('conditionsDesc5').innerHTML = weatherData.list[4].weather[0].description;
+    document.getElementById("minTemp5").innerHTML = weatherData.list[4].temp.max+"&deg;";
+    document.getElementById("maxTemp5").innerHTML = weatherData.list[4].temp.min+"&deg;";
+}
